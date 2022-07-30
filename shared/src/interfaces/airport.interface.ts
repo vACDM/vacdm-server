ac74d02c33b91765e5ba2be3c1ed45511deac1a3
Fionn <fionnsperath@outlook.de>
2022-07-30 23:22:07 +0200

    defined airport and pilot models

--- shared/src/interfaces/airport.interface.ts
+++ shared/src/interfaces/airport.interface.ts
@@ -1,3 +1,22 @@
-interface Airport {}
+interface Airport {
+  icao: string;
+
+  standard_taxitime: number;
+
+  taxizones: {
+    polygon: string[];
+    taxitimes: {
+      rwy_designator: string;
+      minutes: number;
+    }[];
+    label: string;
+    taxiout: boolean;
+  }[];
+
+  capacities: {
+    rwy_designator: string;
+    capacity: number;
+  }[];
+}
 
 export default Airport;
--- shared/src/interfaces/pilot.interface.ts
+++ shared/src/interfaces/pilot.interface.ts
@@ -1,3 +1,69 @@
-interface Pilot {}
+interface Pilot {
+  callsign: string;
+
+  position: {
+    lat: number;
+    lon: number;
+  };
+
+  vacdm: {
+    eobt: Date;
+    tobt: Date;
+
+    exot: Number;
+    manual_exot: boolean;
+
+    tsat: Date;
+
+    ctot: Date;
+    ttot: Date;
+
+    asat: Date;
+    aobt: Date;
+
+    delay: Number;
+    prio: Number;
+
+    sug: Date;
+    pbg: Date;
+    txg: Date;
+
+    taxizone: string;
+
+    blockAssignment: Date;
+    blockID: Number;
+    block_rwy_designator: String;
+  };
+
+  flightplan: {
+    flight_rules: string;
+
+    departure: string;
+    arrival: string;
+  };
+
+  clearance: {
+    dep_rwy: string;
+    sid: string;
+    initial_climb: string;
+    assigned_squawk: string;
+    current_squawk: string;
+  };
+
+  log: {
+    time: Date;
+    namespace: string;
+    action: string;
+    data: {
+      [key: string]: any;
+    };
+  }[];
+
+  inactive: boolean;
+
+  // mongoose fields
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 export default Pilot;
