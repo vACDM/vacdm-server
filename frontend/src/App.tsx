2bdf4555739ec5dca1272051f264b5ce7ccc055a
Moritz <moritz.frenken@gmail.com>
2022-10-08 10:12:02 +0200

    format Code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,27 +25,33 @@ function App() {
   return (
     <>
       <Router>
-          <AuthProvider >
-            <Navbar />
-            <div className="mt-2">
-              <Suspense fallback={<Loading />}>
-                <Routes>
-                  <Route path="/airports" element={<AirportsTable />} />
-                  <Route path="/airports/:icao" element={<AirportDetails />} />
-                  <Route path="/airports/:icao/edit" element={<AirportDetailsEditor />} />
-                  <Route path="/vdgs/:callsign" element={<Vdgs />} />
-                  <Route path="/debug/:callsign" element={<Debug />} />
-                  <Route path="/departure-blocks/:icao" element={<DepartureBlocks />} />
-                  <Route path="/logo" element={<Loading />} />
-                  <Route path="/flow-management" element={<FlowManagement />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/landingpage" element={<Landingpage />} />
-                  <Route path="/" element={<PilotsTable />} />
-                  <Route path="*" element={<Navigate to="/" />} />
-                </Routes>
-              </Suspense>
-            </div>
-          </AuthProvider>
+        <AuthProvider>
+          <Navbar />
+          <div className="mt-2">
+            <Suspense fallback={<Loading />}>
+              <Routes>
+                <Route path="/airports" element={<AirportsTable />} />
+                <Route path="/airports/:icao" element={<AirportDetails />} />
+                <Route
+                  path="/airports/:icao/edit"
+                  element={<AirportDetailsEditor />}
+                />
+                <Route path="/vdgs/:callsign" element={<Vdgs />} />
+                <Route path="/debug/:callsign" element={<Debug />} />
+                <Route
+                  path="/departure-blocks/:icao"
+                  element={<DepartureBlocks />}
+                />
+                <Route path="/logo" element={<Loading />} />
+                <Route path="/flow-management" element={<FlowManagement />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/landingpage" element={<Landingpage />} />
+                <Route path="/" element={<PilotsTable />} />
+                <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
+            </Suspense>
+          </div>
+        </AuthProvider>
       </Router>
     </>
   );
