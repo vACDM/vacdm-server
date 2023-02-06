613cd72a51f7e0dfcff139f0434fda1de02d9650
Moritz <moritz.frenken@gmail.com>
2023-02-06 16:17:05 +0100

    vdgs with niki

--- frontend/src/components/Vdgs.tsx
+++ frontend/src/components/Vdgs.tsx
@@ -1,51 +1,40 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Navigate, useNavigate, useParams } from 'react-router-dom';
-import dayjs from 'dayjs';
-import { Toast } from 'primereact/toast';
+import React, { useState, useEffect, useRef } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import dayjs from "dayjs";
+import { Toast } from "primereact/toast";
 
-import PilotService from '../services/PilotService';
-import TimeUtils from '../utils/time';
-import Pilot from '@shared/interfaces/pilot.interface';
-import User from '@shared/interfaces/user.interface';
-import { Card } from 'primereact/card';
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import VdgsService from 'services/VdgsService';
-import AuthService from 'services/AuthService';
-import DatafeedService from 'services/DatafeedService';
+import PilotService from "../services/PilotService";
+import TimeUtils from "../utils/time";
+import Pilot from "@shared/interfaces/pilot.interface";
+import User from "@shared/interfaces/user.interface";
+import { Card } from "primereact/card";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import VdgsService from "services/VdgsService";
+import AuthService from "services/AuthService";
+import DatafeedService from "services/DatafeedService";
 
 const Vdgs = () => {
   const navigate = useNavigate();
   const { callsign } = useParams();
-  const [user, setUser] = useState<User>();
-  const [pilot, setPilot] = useState<Pilot>();
+
+  const [pilot, setPilot] = useState<Pilot | undefined>(undefined);
   const [loading, setLoading] = useState(true);
   const [loadingTobt, setLoadingTobt] = useState(false);
-  const [inputTextValue, setinputTextValue] = useState('');
-  const [validity, setValidity] = useState('');
-  const [wrongFormat, setwrongFormat] = useState('');
+  const [inputTextValue, setinputTextValue] = useState("");
+  const [validity, setValidity] = useState("");
+  const [wrongFormat, setwrongFormat] = useState("");
   const toast: any = useRef(null);
 
   useEffect(() => {
-    setwrongFormat('');
-    setValidity('');
+    setwrongFormat("");
+    setValidity("");
     async function loadData() {
-      if (!callsign || callsign === '') {
-        //showInfo();
-        await checkPilot();
-        return;
-      }
-
-      try {
-        const data: Pilot = await PilotService.getPilot(callsign);
-
-        setPilot(data);
-        setLoading(false);
-      } catch (e) {}
+      await checkPilot();
     }
-    let intervalId = setInterval(loadData, 10000);
 
     loadData();
+    let intervalId = setInterval(loadData, 10000);
 
     return () => clearInterval(intervalId);
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
@@ -53,23 +42,21 @@ const Vdgs = () => {
   async function checkPilot() {
     try {
       const user: User = await AuthService.getProfile();
-      setUser(user);
 
       const pilot: Pilot = await DatafeedService.getPilotFromCid(
         user.apidata.cid
       );
 
-      if (pilot || pilot !== '') {
-        navigate('/vdgs/' + pilot.callsign);
-      }
+      setPilot(pilot);
+      setLoading(false);
     } catch (e) {}
   }
 
   const showInfo = () => {
     toast.current.show({
-      severity: 'info',
-      summary: 'No Callsign found',
-      detail: 'Trying again in 10 sec.',
+      severity: "info",
+      summary: "No Callsign found",
+      detail: "Trying again in 10 sec.",
       life: 3000,
     });
   };
@@ -77,15 +64,15 @@ const Vdgs = () => {
   function vdgsColorController(time: Date | undefined) {
     let now = dayjs().second(0);
     let tsat = dayjs(time).second(0);
-    return now.diff(tsat, 'minute') > 5 ? 'textColorRed' : '';
+    return now.diff(tsat, "minute") > 5 ? "textColorRed" : "";
   }
 
   function buttonDisabled(time: Date | undefined) {
     let now = dayjs().second(0);
     let tobt = dayjs(time).second(0);
-    console.log(now.diff(tobt, 'minute'));
+    console.log(now.diff(tobt, "minute"));
 
-    return now.diff(tobt, 'minute') >= -10 ? true : false;
+    return now.diff(tobt, "minute") >= -10 ? true : false;
   }
 
   async function updateTobt() {
@@ -95,86 +82,86 @@ const Vdgs = () => {
       p.vacdm.tsat = dayjs(-1).toDate();
     }
 
-    let regex = new RegExp('^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$');
+    let regex = new RegExp("^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$");
     if (regex.test(inputTextValue)) {
       setLoadingTobt(true);
       setPilot(p);
-      setwrongFormat('');
-      setValidity('');
+      setwrongFormat("");
+      setValidity("");
       setinputTextValue(inputTextValue);
       await VdgsService.updateTobt(inputTextValue, callsign)
         .then(() => {
           setLoadingTobt(false);
-          setinputTextValue('');
+          setinputTextValue("");
         })
         .catch(() => {
-          setwrongFormat('Unauthorized!');
+          setwrongFormat("Unauthorized!");
           setLoadingTobt(false);
         });
     } else {
-      setValidity('p-invalid');
-      setwrongFormat('Allowed Format: HHMM');
+      setValidity("p-invalid");
+      setwrongFormat("Allowed Format: HHMM");
     }
   }
 
   return (
     <>
       <Toast ref={toast} />
-      <div className='grid m-2'>
-        <div className='lg:col'></div>
-        <div className='lg:col md:col-6'>
-          <div className='vdgs-container text-center'>
+      <div className="grid m-2">
+        <div className="lg:col"></div>
+        <div className="lg:col md:col-6">
+          <div className="vdgs-container text-center">
             {loading ? (
               <div>
                 SEARCHING FOR <br />
                 CALLSIGN...
               </div>
             ) : (
-              <div className='flex align-items-center justify-content-center'>
-                <div className='inline-block m-2'>
-                  <div className='text-center'>{pilot?.callsign}</div>
-                  <div className='text-center'>
+              <div className="flex align-items-center justify-content-center">
+                <div className="inline-block m-2">
+                  <div className="text-center">{pilot?.callsign}</div>
+                  <div className="text-center">
                     TOBT {TimeUtils.formatTime(pilot?.vacdm.tobt)} UTC
                   </div>
-                  <div className='text-center'>
+                  <div className="text-center">
                     TSAT {TimeUtils.formatTime(pilot?.vacdm.tsat)} UTC
                   </div>
-                  <div className='text-center'>
+                  <div className="text-center">
                     <span className={vdgsColorController(pilot?.vacdm?.tsat)}>
                       {TimeUtils.calculateVdgsDiff(pilot?.vacdm?.tsat)}
                     </span>
                   </div>
-                  <div className='text-center'>
+                  <div className="text-center">
                     PLANNED RWY {pilot?.clearance?.dep_rwy}
                   </div>
-                  <div className='text-center'>SID {pilot?.clearance?.sid}</div>
+                  <div className="text-center">SID {pilot?.clearance?.sid}</div>
                 </div>
               </div>
             )}
           </div>
         </div>
-        <div className='lg:col md:col-6'>
+        <div className="lg:col md:col-6">
           <Card>
             <h5>Update TOBT (UTC-Time when ready for Pushback)</h5>
-            <div className='flex flex-wrap gap-2'>
-              <div className=''>
+            <div className="flex flex-wrap gap-2">
+              <div className="">
                 <InputText
                   className={validity}
-                  placeholder='HHMM'
+                  placeholder="HHMM"
                   value={inputTextValue}
                   onChange={(e) => setinputTextValue(e.target.value)}
-                  aria-describedby='tobt-help'
+                  aria-describedby="tobt-help"
                 ></InputText>
-                <small id='tobt-help' className='block'>
+                <small id="tobt-help" className="block">
                   {wrongFormat}
                 </small>
               </div>
               <Button
-                label='Submit TOBT'
-                className=''
+                label="Submit TOBT"
+                className=""
                 loading={loadingTobt}
                 onClick={updateTobt}
-                disabled={(!callsign || callsign === '') ? true : false}
+                disabled={!callsign || callsign === "" ? true : false}
               ></Button>
             </div>
           </Card>
