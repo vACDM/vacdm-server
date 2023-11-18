1a89bd04d91ba18b211982d610d71c57d14c1d62
Fionn <fionnsperath@outlook.de>
2023-11-18 14:38:40 +0100

    remove unused import

--- src/backend/pilot/pilot.controller.ts
+++ src/backend/pilot/pilot.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import Joi from 'joi';
 import { JoiPipe } from 'nestjs-joi';
 
 import { PilotDto, PilotCallsignValidator } from './pilot.dto';
