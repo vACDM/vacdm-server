22bac3262dce7bb01095f3194db84327f90a631d
Fionn <fionnsperath@outlook.de>
2023-12-14 21:12:21 +0100

    reconfigure settings

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -1,93 +1,120 @@
-import { FrontendSettings, PluginSettings } from '@/shared/interfaces/config.interface';
+import Joi from 'joi';
 
-interface VacdmConfig {
-  mongoUri: string;
-  port: number;
-  role: 'API' | 'WORKER';
+import { VacdmAppConfig } from '@/shared/interfaces/config.interface';
 
-  pluginSettings: PluginSettings;
-  frontendSettings: FrontendSettings;
-
-  timeframes: {
-    timeSinceLastSeen: number;
-    timeSinceInactive: number;
-    timeSinceLastLogin: number;
-  };
-
-  logging: {
-    levelConsole: string;
-    levelFile: string;
-  };
-
-  eventUrl: string;
-  eventPrio: number;
-  eventPullInterval: number;
-
-  clientId: string;
-  clientSecret: string;
-  vaccAuthType: string;
-  vaccAuthUrl: string;
-  vaccAuthToken: string;
-  publicUrl: string;
-  vatsimAuthUrl: string;
-  jwtSecret: string;
-
-  frontendProxy: string;
+interface VacdmConfigEnv {
+  MONGO_URI: string;
+  
+  SERVER_NAME: string;
+  
+  VATSIM_AUTH_URL: string;
+  CLIENT_ID: string;
+  CLIENT_SECRET: string;
+  
+  PORT: number;
+  
+  ALLOW_SIM: string | void;
+  ALLOW_OBS_MASTER: string | void;
+  
+  TIME_LAST_SEEN: number;
+  TIME_INACTIVE: number;
+  TIME_LAST_LOGIN: number;
+  
+  LOG_LEVEL_CONSOLE: string;
+  LOG_LEVEL_FILE: string;
+  
+  EVENT_URL: string | void;
+  EVENT_PRIO: string;
+  EVENT_PULL_INTERVAL: string;
+  
+  PUBLIC_URL: string;
+  JWT_SECRET: string;
+  FRONTEND_PROXY: string | void;
 }
 
-export default function getAppConfig(): VacdmConfig {
+const configValidationResult = Joi.object<VacdmConfigEnv>({
+  MONGO_URI: Joi.string().required(),
+
+  SERVER_NAME: Joi.string().default('vACDM Server'),
+
+  VATSIM_AUTH_URL: Joi.string().default('https://auth.vatsim.net'),
+  CLIENT_ID: Joi.string().required(),
+  CLIENT_SECRET: Joi.string().required(),
+
+  PORT: Joi.number().default(3000),
+
+  ALLOW_SIM: Joi.string().optional(),
+  ALLOW_OBS_MASTER: Joi.string().optional(),
+
+  // TODO: maybe use timestring module?
+  TIME_LAST_SEEN: Joi.number().default(5),
+  TIME_INACTIVE: Joi.number().default(5),
+  TIME_LAST_LOGIN: Joi.number().default(48),
+
+  LOG_LEVEL_CONSOLE: Joi.string().default('http'),
+  LOG_LEVEL_FILE: Joi.string().default('info'),
+
+  EVENT_URL: Joi.string().optional(),
+  EVENT_PRIO: Joi.string().default(5),
+  EVENT_PULL_INTERVAL: Joi.string().default(5),
+
+  PUBLIC_URL: Joi.string().required(),
+  JWT_SECRET: Joi.string().default('super-secret-secret'),
+  FRONTEND_PROXY: Joi.string().optional(),
+}).unknown(true).validate(process.env);
+const validatedEnv: VacdmConfigEnv = configValidationResult.value;
+
+if (configValidationResult.error) {
+  // eslint-disable-next-line no-console
+  console.error(`\x1b[30;41mFailed to start Application: \x1b[35;37m${configValidationResult.error.message}\x1b[0m`);
+  throw configValidationResult.error;
+}
+
+export default function getAppConfig(): VacdmAppConfig {
   const options = {
-    serverName: process.env.SERVER_NAME ?? 'vACDM Server',
-    vatsimAuthUrl: process.env.VATSIM_AUTH_URL ?? 'https://auth.vatsim.net',
-    vatsimAuthClientId: process.env.CLIENT_ID ?? '',
-    vaccLogoUrl: process.env.VACC_LOGO_URL ?? '',
-    vaccImprintUrl: process.env.VACC_IMPRINT_URL ?? '',
+    serverName: validatedEnv.SERVER_NAME ?? 'vACDM Server',
+    vatsimAuthUrl: validatedEnv.VATSIM_AUTH_URL ?? 'https://auth.vatsim.net',
+    vatsimAuthClientId: validatedEnv.CLIENT_ID ?? '',
   };
 
   return {
-    mongoUri: process.env.MONGO_URI || '',
-    port: Number(process.env.PORT) || 3000,
-    role: process.env.ROLE != 'WORKER' ? 'API' : 'WORKER',
+    mongoUri: validatedEnv.MONGO_URI,
+    port: validatedEnv.PORT,
 
     pluginSettings: {
       serverName: options.serverName,
-      allowSimSession: process.env.ALLOW_SIM == 'true' ?? false,
-      allowObsMaster: process.env.ALLOW_OBS_MASTER == 'true' ?? false,
+      allowSimSession: validatedEnv.ALLOW_SIM == 'true' ?? false,
+      allowObsMaster: validatedEnv.ALLOW_OBS_MASTER == 'true' ?? false,
     },
 
     frontendSettings: {
       serverName: options.serverName,
       vatsimAuthUrl: options.vatsimAuthUrl,
       vatsimAuthClientId: options.vatsimAuthClientId,
-      vaccLogoUrl: options.vaccLogoUrl,
-      vaccImprintUrl: options.vaccImprintUrl,
     },
 
     timeframes: {
-      timeSinceLastSeen: Number(process.env.TIME_LAST_SEEN || 5) * 60 * 1000,
-      timeSinceInactive: Number(process.env.TIME_INACTIVE || 5) * 60 * 1000,
-      timeSinceLastLogin: Number(process.env.TIME_LAST_LOGIN || 48) * 60 * 60 * 1000,
+      timeSinceLastSeen: Number(validatedEnv.TIME_LAST_SEEN || 5) * 60 * 1000,
+      timeSinceInactive: Number(validatedEnv.TIME_INACTIVE || 5) * 60 * 1000,
+      timeSinceLastLogin: Number(validatedEnv.TIME_LAST_LOGIN || 48) * 60 * 60 * 1000,
     },
 
     logging: {
-      levelConsole: process.env.LOG_LEVEL_CONSOLE || 'http',
-      levelFile: process.env.LOG_LEVEL_FILE || 'info',
+      levelConsole: validatedEnv.LOG_LEVEL_CONSOLE || 'http',
+      levelFile: validatedEnv.LOG_LEVEL_FILE || 'info',
     },
 
-    eventUrl: process.env.EVENT_URL || 'https://slots.vatsim-germany.org/api/events/',
-    eventPrio: Number(process.env.EVENT_PRIO) || 5,
-    eventPullInterval: Number(process.env.EVENT_PULL_INTERVAL || 5),
+    eventUrl: validatedEnv.EVENT_URL || 'https://slots.vatsim-germany.org/api/events/',
+    eventPrio: Number(validatedEnv.EVENT_PRIO) || 5,
+    eventPullInterval: Number(validatedEnv.EVENT_PULL_INTERVAL || 5),
 
     vatsimAuthUrl: options.vatsimAuthUrl,
     clientId: options.vatsimAuthClientId,
-    clientSecret: process.env.CLIENT_SECRET ?? '',
-    vaccAuthType: process.env.VACC_AUTH_TYPE ?? '',
-    vaccAuthUrl: process.env.VACC_AUTH_URL ?? '',
-    vaccAuthToken: process.env.VACC_AUTH_TOKEN ?? '',
+    clientSecret: validatedEnv.CLIENT_SECRET ?? '',
 
-    publicUrl: process.env.PUBLIC_URL ?? '',
-    jwtSecret: process.env.JWT_SECRET ?? 'super-secret-secret!',
+    publicUrl: validatedEnv.PUBLIC_URL ?? '',
+    jwtSecret: validatedEnv.JWT_SECRET ?? 'super-secret-secret!',
 
-    frontendProxy: process.env.FRONTEND_PROXY ?? '',
+    frontendProxy: validatedEnv.FRONTEND_PROXY ?? '',
   };
 }
