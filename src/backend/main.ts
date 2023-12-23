af141576c0395782ea0b258ed2eec49a34ecefde
Fionn <fionnsperath@outlook.de>
2023-12-23 16:54:28 +0100

    apply http log only to api endpoints

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import { Request } from 'express';
 import morgan from 'morgan';
 import { WinstonModule } from 'nest-winston';
 
@@ -13,7 +14,17 @@ async function bootstrap() {
     }),
   });
 
-  app.use(morgan('short', { stream: { write: m => logger.http(m.trim()) } }));
+  const logMiddleware = morgan('short', {
+    stream: { write: m => logger.http(m.trim()) },
+  });
+
+  app.use((req: Request, res, next) => {
+    if (req.originalUrl.startsWith('/api')) {
+      return logMiddleware(req, res, next);
+    }
+
+    next();
+  });
 
   app.use(cookieParser());
 
