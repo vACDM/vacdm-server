b22f55e600a7df321ef2d204086356467297ea73
Fionn <fionnsperath@outlook.de>
2024-03-24 12:50:18 +0100

    add writing indexes to mongodb on app start

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import { Request } from 'express';
+import mongoose from 'mongoose';
 import morgan from 'morgan';
 import { WinstonModule } from 'nest-winston';
 
@@ -37,6 +38,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  (async () => {
+    try {
+      await mongoose.syncIndexes();
+      logger.info('Synced indexes to MongoDB!');
+    } catch (error) {
+      logger.warn('Failed to sync indexes to MongoDB: %o', error);
+    }
+  })();
 
   await app.listen(3000);
 }
