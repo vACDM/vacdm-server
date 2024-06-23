665c969d95f5664b4e2e5247800c3d690514734c
frenkii <moritz.frenken@gmail.com>
2024-06-23 23:00:43 +0200

    wip

--- src/backend/etfms/etfms.service.ts
+++ src/backend/etfms/etfms.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import Agenda from 'agenda';
+
+import { ECFMP_MEASURE_MODEL, EcfmpMeasureModel } from '../ecfmp/ecfmp-measure.model';
+import { AGENDA_PROVIDER } from '../schedule.module';
 
 @Injectable()
-export class EtfmsService {}
+export class EtfmsService {
+  constructor(
+    @Inject(ECFMP_MEASURE_MODEL) private ecfmpMeasureModel: EcfmpMeasureModel,
+    @Inject(AGENDA_PROVIDER) private agenda: Agenda,
+  ) {
+    this.agenda.define('ETFMS_assignMeasures', this.assignMeasures.bind(this));
+    this.agenda.every('1 minute', 'ETFMS_assignMeasures');
+  }
+
+  async assignMeasures() {
+
+  }
+
+
+}
