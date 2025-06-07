2f36c06cd8880d7ab58a164f3372eeef056711ba
Fionn <fionnsperath@outlook.de>
2025-06-07 23:01:42 +0200

    make eobt in test script change based on index

--- rein.js
+++ rein.js
@@ -3,6 +3,14 @@
 
 const callsigns = Array(10).fill(null).map((_, i) => i + 1);
 
+function makeDate(i) {
+  const d = new Date();
+  
+  d.setMinutes(d.getMinutes() + 30 + i);
+  
+  return d;
+}
+
 (async () => {
   for (const cs of callsigns) {
     await fetch('http://localhost:3030/api/v1/pilots', {
@@ -11,7 +19,7 @@ const callsigns = Array(10).fill(null).map((_, i) => i + 1);
         "callsign": `TST${cs}`,
         "position": {"lat": 1, "lon": 1},
         "flightplan": {"adep":"XXXX", "ades": "ZZZz"},
-        "vacdm": {"eobt": -1, "tobt": -1},
+        "vacdm": {"eobt": makeDate(cs), "tobt": -1},
         "clearance": {"dep_rwy": "187", "sid": "PIMML5L"}
       }),
       headers: {
