59dbf02c35ef46d1557f2535ed4926bb343f1c02
Fionn <fionnsperath@outlook.de>
2025-07-29 17:51:16 +0200

    make admins definable in env vars

--- src/backend/user/user.model.ts
+++ src/backend/user/user.model.ts
@@ -1,6 +1,8 @@
 import mongoose, { HydratedDocument, Model } from 'mongoose';
 
+import getAppConfig from '../config';
 import { DB_PROVIDER } from '../database.module';
+import logger from '../logger';
 
 import User from '@/shared/interfaces/user.interface';
 
@@ -8,18 +10,24 @@ export const USER_MODEL = 'USER_MODEL';
 export type UserModel = Model<User>;
 export type UserDocument = HydratedDocument<User>;
 
+const { admins } = getAppConfig();
+
+logger.debug('admins: %o', admins);
+
 const UserSchema = new mongoose.Schema<User>({
   cid: { type: Number, unique: true },
 
   firstName: { type: String, default: '' },
   lastName: { type: String, default: '' },
 
-  admin: { type: Boolean, default: false },
+  // admin: { type: Boolean, default: false },
   hasAtcRating: { type: Boolean, default: false },
   banned: { type: Boolean, default: false },
 
   roles: [{ type: String }],
-}, { timestamps: true });
+}, { timestamps: true, toJSON: { virtuals: ['admin'] }, toObject: { virtuals: ['admin'] } });
+
+UserSchema.virtual('admin').get(function (this: UserDocument) { return !!admins[this.cid]; });
 
 export const UserProvider = {
   provide: USER_MODEL,
